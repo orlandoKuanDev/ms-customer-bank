398b51920300b96f2c7fd21a3b6862f3dedd4724
OrlandoKuan <orlandokuanb@gmail.com>
2021-08-03 23:47:22 -0500

    Refactor POM and entities

--- src/main/java/com/bootcamp/mscustomer/models/entities/CustomerType.java
+++ src/main/java/com/bootcamp/mscustomer/models/entities/CustomerType.java
@@ -4,32 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-//import org.hibernate.annotations.NaturalId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@Document(collation = "customers")
+@Document(collation = "customer_type")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Customer {
+public class CustomerType {
     @Id
     private String id;
     @NotBlank
-    @Size(max = 140)
     private String name;
-    private String code;
-    //@NaturalId
-    private String iban;
-    private String names;
-    private String surname;
-    private String phone;
-    private String address;
-    @Valid
-    private CustomerType customerType;
 }
