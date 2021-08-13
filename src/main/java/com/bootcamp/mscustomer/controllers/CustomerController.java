c0f48043feaff71c5c5abe03974beef6a0912afa
rosasjose96 <34173505+rosasjose96@users.noreply.github.com>
2021-08-12 20:18:54 -0500

    refactor

--- src/main/java/com/bootcamp/mscustomer/controllers/CustomerController.java
+++ src/main/java/com/bootcamp/mscustomer/controllers/CustomerController.java
@@ -9,8 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.reactive.Response;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
