44d90510b35b50960e23e0993330924a158d8d9f
Orlando Kuan <orlandojose.kuanbecerra@everis.nttdata.com>
2021-08-28 09:25:52 -0500

    REFACTOR add kafka configuration, and add CustomerConsumer

---
+++ src/main/java/com/bootcamp/mscustomer/topic/consumer/CustomerConsumer.java
@@ -0,0 +1,38 @@
+package com.bootcamp.mscustomer.topic.consumer;
+
+import com.bootcamp.mscustomer.models.entities.Customer;
+import com.bootcamp.mscustomer.services.ICustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import reactor.core.Disposable;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class CustomerConsumer {
+    private final static String CREATE_CUSTOMER_TOPIC = "created-customer-topic";
+    private final static String GROUP_ID = "customer-group";
+
+    private final ICustomerService customerService;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public CustomerConsumer(ICustomerService customerService, ObjectMapper objectMapper) {
+        this.customerService = customerService;
+        this.objectMapper = objectMapper;
+    }
+
+    @KafkaListener( topics = CREATE_CUSTOMER_TOPIC, groupId = GROUP_ID)
+    public Disposable retrieveSavedCustomer(String data) throws Exception {
+        log.info("data from kafka listener (customer) =>"+data);
+        Customer customer= objectMapper.readValue(data, Customer.class );
+
+        return Mono.just(customer)
+                .log()
+                .flatMap(customerService::create)
+                .subscribe();
+    }
+}
