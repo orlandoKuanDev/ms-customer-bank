3e0db6aad0b5f82cdeb8b9318e875418384c2e67
Orlando Kuan <orlandojose.kuanbecerra@everis.nttdata.com>
2021-09-02 17:45:03 -0500

    CHORE add unit testing handler

---
+++ src/test/java/com/bootcamp/mscustomer/handler/CustomerHandlerTest.java
@@ -0,0 +1,12 @@
+package com.bootcamp.mscustomer.handler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerHandlerTest {
+
+    @Test
+    void findAll() {
+    }
+}
